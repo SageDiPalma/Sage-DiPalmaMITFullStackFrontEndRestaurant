68cdebbb97a62bc69ac4bc5c725a32a6c3e373fb
sage.dipalma@gmail.com <sage.dipalma@gmail.com>
2024-06-08 12:52:20 -0400

    added conditional image

--- components/dishes.js
+++ components/dishes.js
@@ -1,6 +1,6 @@
-import {useRouter} from "next/router"
-import {gql,useQuery} from '@apollo/client';
-import {useState, useContext} from 'react'
+import { useRouter } from "next/router"
+import { gql, useQuery } from '@apollo/client';
+import { useState, useContext } from 'react'
 import AppContext from "./context"
 import {
   Button,
@@ -10,14 +10,15 @@ import {
   CardText,
   CardTitle,
   Row,
-  Col} from "reactstrap";
-function Dishes({restId}){
+  Col
+} from "reactstrap";
+function Dishes({ restId }) {
   const [restaurantID, setRestaurantID] = useState()
-  const {addItem} = useContext(AppContext)
+  const { addItem } = useContext(AppContext)
 
   const BE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
-const GET_RESTAURANT_DISHES = gql`
+  const GET_RESTAURANT_DISHES = gql`
   query($id: ID!) {
     restaurant(id: $id) {
       id
@@ -38,7 +39,7 @@ const GET_RESTAURANT_DISHES = gql`
   const router = useRouter();
 
   const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES, {
-    variables: { id: restId},
+    variables: { id: restId },
   });
 
   if (loading) return <p>Loading...</p>;
@@ -47,38 +48,43 @@ const GET_RESTAURANT_DISHES = gql`
 
   let restaurant = data.restaurant;
 
-  if (restId > 0){
+  if (restId > 0) {
 
     return (
       <>
-          {restaurant.dishes.map((res) => (
-            <Col xs="6" sm="4" style={{ padding: 0 }} key={res.id}>
-              <Card style={{ margin: "0 10px" }}>
+        {restaurant.dishes.map((res) => (
+          <Col xs="6" sm="4" style={{ padding: 0 }} key={res.id}>
+            <Card style={{ margin: "0 10px" }}>
+              {res.image && res.image.url ? (
                 <CardImg
                   top={true}
-                  style={{ height: 150, width:150 }}
+                  style={{ height: 150, width: 150 }}
                   src={`${BE_URL}${res.image.url}`}
                 />
-                <CardBody>
-                  <CardTitle><b>{res.name}</b></CardTitle>
-                  <CardText>{res.description}</CardText>
-                </CardBody>
-                <div className="card-footer">
-                  <Button 
-                    outline
-                    color="primary"
-                    onClick = {()=> addItem(res)}
-                  >
-                    + Add To Cart
-                  </Button>
-                </div>
-              </Card>
-            </Col>
-          ))}
-        </>
-        )}
-        else{
-          return <h1> No Dishes</h1>
-        }
-    }
-    export default Dishes
+              ) : (
+                <div style={{ height: 150, width: 150, backgroundColor: '#eaeaea' }}></div>
+              )}
+              <CardBody>
+                <CardTitle><b>{res.name}</b></CardTitle>
+                <CardText>{res.description}</CardText>
+              </CardBody>
+              <div className="card-footer">
+                <Button
+                  outline
+                  color="primary"
+                  onClick={() => addItem(res)}
+                >
+                  + Add To Cart
+                </Button>
+              </div>
+            </Card>
+          </Col>
+        ))}
+      </>
+    )
+  }
+  else {
+    return <h1> No Dishes</h1>
+  }
+}
+export default Dishes
